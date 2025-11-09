37f7ccbb121b2f9589cde7b181236ed4f724ea93
Hugo Pierret <hugo.pierret.hp@gmail.com>
2025-11-09 18:07:19 +0100

    improve first page with logo and footer

--- src/components/UploadDiary.js
+++ src/components/UploadDiary.js
@@ -7,6 +7,8 @@ import {
   Button,
   CircularProgress,
   TextField,
+  Box,
+  Link,
 } from "@material-ui/core";
 import { CloudUploadOutlined } from "@material-ui/icons";
 import CloudDoneOutlinedIcon from "@mui/icons-material/CloudDoneOutlined";
@@ -14,6 +16,7 @@ import { useDropzone } from "react-dropzone";
 import axios from "axios";
 
 const apiUrl = process.env.REACT_APP_API_URL;
+const VERSION = require("../../package.json").version;
 
 const UploadDiary = () => {
   const [file, setFile] = useState(null);
@@ -50,8 +53,6 @@ const UploadDiary = () => {
       setUploading(true);
       try {
         if (username) {
-          console.log(`Fetching diary for user: ${username}`);
-          // Call backend to fetch diary from username
           const response = await axios.post(
             `${apiUrl}/api/fetch-diary`,
             { username },
@@ -84,77 +85,107 @@ const UploadDiary = () => {
   };
 
   return (
-    <Container className="upload-diary">
-      <div className="upload-card">
-        <Typography variant="h4" className="title">
-          Upload Letterboxd Diary
-        </Typography>
-        <TextField
-          className="username-field"
-          label="Letterboxd Username"
-          variant="outlined"
-          fullWidth
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          onKeyUp={(e) => {
-            if (e.key === "Enter") handleUpload();
-          }}
-          placeholder="Enter your Letterboxd username"
-        />
-        <Typography variant="body2" className="or-text">
-          Or upload a CSV file:
-        </Typography>
-        <div {...getRootProps()} className="dropzone">
-          <input {...getInputProps()} />
-          {file ? (
-            <>
-              <CloudDoneOutlinedIcon className="dropzone-icon" />
-              <Typography variant="h6" className="dropzone-text">
-                Your file has been uploaded: {file.name}
-              </Typography>
-            </>
-          ) : (
-            <>
-              <CloudUploadOutlined className="dropzone-icon" />
-              <Typography variant="h6" className="dropzone-text">
-                Drag and drop a CSV file here or click to select
-              </Typography>
-            </>
-          )}
+    <Box>
+      <Container className="upload-diary">
+        <div className="upload-card">
+          <div className="logo-container">
+            <img src="/icon.svg" alt="logo" className="logo" />
+          </div>
+          <Typography variant="h4" className="title">
+            Upload Letterboxd Diary
+          </Typography>
+          <TextField
+            className="username-field"
+            label="Letterboxd Username"
+            variant="outlined"
+            fullWidth
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") handleUpload();
+            }}
+            placeholder="Enter your Letterboxd username"
+          />
+          <Typography variant="body2" className="or-text">
+            Or upload a CSV file:
+          </Typography>
+          <div {...getRootProps()} className="dropzone">
+            <input {...getInputProps()} />
+            {file ? (
+              <>
+                <CloudDoneOutlinedIcon className="dropzone-icon" />
+                <Typography variant="h6" className="dropzone-text">
+                  Your file has been uploaded: {file.name}
+                </Typography>
+              </>
+            ) : (
+              <>
+                <CloudUploadOutlined className="dropzone-icon" />
+                <Typography variant="h6" className="dropzone-text">
+                  Drag and drop a CSV file here or click to select
+                </Typography>
+              </>
+            )}
+          </div>
+          <Grid container justify="center" className="progress-container">
+            {uploading ? (
+              <CircularProgress />
+            ) : (
+              <Button
+                className="submit-button"
+                variant="contained"
+                onClick={handleUpload}
+                disabled={!username && !file}
+              >
+                SUBMIT
+              </Button>
+            )}
+          </Grid>
         </div>
-        <Grid container justify="center" className="progress-container">
-          {uploading ? (
-            <CircularProgress />
-          ) : (
-            <Button
-              className="submit-button"
-              variant="contained"
-              onClick={handleUpload}
-              disabled={!username && !file}
-            >
-              SUBMIT
-            </Button>
-          )}
-        </Grid>
-      </div>
-      <Typography variant="body1" className="overview-text">
-        This tool allows you to easily import your Letterboxd diary and select
-        your favorite movie poster. Follow these simple steps:
-        <br />
-        <strong>1. Enter Your Letterboxd Username:</strong> Type your Letterboxd
-        username to automatically fetch your diary.
-        <br />
-        <strong>2. Or Upload Your CSV File:</strong> If you prefer, you can
-        still upload your diary.csv file directly.
-        <br />
-        <strong>3. Automatic Processing:</strong> Once submitted, the
-        application will process your diary.
-        <br />
-        <strong>4. Poster Selection:</strong> After processing, you'll be
-        redirected to the Poster Selector page, where you can browse and choose
-        your favorite movie poster based on your Letterboxd diary.
-      </Typography>
-    </Container>
+        <Typography variant="body1" className="overview-text">
+          This tool allows you to easily import your Letterboxd diary and select
+          your favorite movie poster. Follow these simple steps:
+          <br />
+          <strong>1. Enter Your Letterboxd Username:</strong> Type your
+          Letterboxd username to automatically fetch your diary.
+          <br />
+          <strong>2. Or Upload Your CSV File:</strong> If you prefer, you can
+          still upload your diary.csv file directly.
+          <br />
+          <strong>3. Automatic Processing:</strong> Once submitted, the
+          application will process your diary.
+          <br />
+          <strong>4. Poster Selection:</strong> After processing, you'll be
+          redirected to the Poster Selector page, where you can browse and
+          choose your favorite movie poster based on your Letterboxd diary.
+        </Typography>
+      </Container>
+      <Box
+        component="footer"
+        className="footer"
+      >
+        <Box
+          className="footer-text"
+        >
+          <Typography variant="body2" style={{ opacity: 0.7 }}>
+            v{VERSION}
+          </Typography>
+          <Typography variant="body2" style={{ opacity: 0.5 }}>
+            •
+          </Typography>
+          <Link
+            href="https://github.com/Hugyouu/Letterboxd-Diary-Posters-Picker"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+            onMouseEnter={(e) => (e.currentTarget.style.opacity = 1)}
+            onMouseLeave={(e) => (e.currentTarget.style.opacity = 0.7)}
+          >
+            <span>Hugyouu</span>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
